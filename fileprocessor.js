144ac940efc1a8c667328ffd7e57d86e351ed2c4
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-08 11:09:09 +0200

    used callback-based fs.readFile to use Promises for async file handling

---
+++ fileprocessor.js
@@ -0,0 +1,10 @@
+
+const fs = require('fs').promises
+
+fs.readFile('sapmple.txt', utf8).then((data) =>{
+    console.log("File content:", data);
+    
+}).catch((err) =>{
+    console.log("Error reading file", err);
+    
+})
